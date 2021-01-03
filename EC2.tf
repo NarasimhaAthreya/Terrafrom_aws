b49f6bce770b604313546f08553c7fdc3d016307
Narasimha Athreya <athreya619@gmail.com>
2021-01-03 13:00:16 +0530

    Added terraform details

--- EC2.tf
+++ EC2.tf
@@ -32,6 +32,70 @@ resource "aws_instance" "ec2_terraform" {
   }
 }
 
+
+resource "aws_lambda_function" "terraform_lamda" {
+
+  filename         = "app.zip"
+  function_name    = "terraform_lamda"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "app.handlers"
+  source_code_hash = filebase64sha256("app.zip")
+  runtime          = "nodejs12.x"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+
+resource "aws_api_gateway_rest_api" "terraform_apigateway" {
+  name        = "terraform_apigateway"
+  description = "This is my API for demonstration purposes"
+
+}
+
+resource "aws_api_gateway_resource" "terraform_resource" {
+  rest_api_id = aws_api_gateway_rest_api.terraform_apigateway.id
+  parent_id   = aws_api_gateway_rest_api.terraform_apigateway.root_resource_id
+  path_part   = "api"
+}
+
+resource "aws_api_gateway_method" "terraform_method" {
+  rest_api_id   = aws_api_gateway_rest_api.terraform_apigateway.id
+  resource_id   = aws_api_gateway_resource.terraform_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+resource "aws_api_gateway_integration" "terraform_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.terraform_apigateway.id
+  resource_id             = aws_api_gateway_resource.terraform_resource.id
+  http_method             = aws_api_gateway_method.terraform_method.http_method
+  integration_http_method = "GET"
+  uri                     = aws_lambda_function.terraform_lamda.invoke_arn
+}
+
+resource "aws_api_gateway_method_response" "terraform_response" {
+  rest_api_id = aws_api_gateway_rest_api.terraform_apigateway.id
+  resource_id = aws_api_gateway_resource.terraform_resource.id
+  http_method = aws_api_gateway_method.terraform_method.method
+  status_code = "200"
+}
 output "Instance_IP" {
 
   value = aws_instance.ec2_terraform
